13edb0ea597494c40297ef03e2dc7b187ce70df2
davidogle <the_davester_808@hotmail.com>
2016-05-16 11:26:44 +0100

    Add table with search (1)

--- Planit_Food/grails-app/views/index.gsp
+++ Planit_Food/grails-app/views/index.gsp
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" ng-app>
+<html lang="en" ng-app="planitFood">
 <head>
     <meta charset="utf-8" />
     <title>Planit Food</title>
@@ -7,10 +7,39 @@
     <g:javascript src="lib/angular.min.js"/>
     <g:javascript src="lib/angular-route.min.js"/>
     <g:javascript src="lib/bootstrap.min.js"/>
+    <!--Controllers-->
+    <g:javascript src="ng-app/controllers/planitFood.controller.js"/>
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" />
     <link href="//cdn.rawgit.com/cornflourblue/angular-registration-login-example/master/app-content/app.css" rel="stylesheet" />
 </head>
-<body>
-     <p>Nothing here {{'yet' + '!'}}</p>
+<body ng-controller="PhoneListCtrl">
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-2">
+      <!--Sidebar content-->
+
+      Search: <input ng-model="query">
+
+    </div>
+    <div class="col-md-10">
+      <!--Body content-->
+
+      <ul class="phones">
+        <li ng-repeat="phone in phones | filter:query">
+          {{phone.name}}
+          <p>{{phone.snippet}}</p>
+        </li>
+      </ul>
+
+    </div>
+  </div>
+</div>
+<br>
+    
+    <table>
+  <tr><th>row number</th></tr>
+  <tr ng-repeat="i in [0, 1, 2, 3, 4, 5, 6, 7]"><td>{{i}}</td></tr>
+</table>
+    
 </body>
 </html>
--- Planit_Food/web-app/js/ng-app/controllers/planitFood.controller.js
+++ Planit_Food/web-app/js/ng-app/controllers/planitFood.controller.js
@@ -1,37 +1,12 @@
-/* 
- * The Login Controller clears the user credentials on load which logs the user out if they were logged in.
- */
-(function () {
-    'use strict';
- 
-    angular
-        .module('app')
-        .controller('LoginController', LoginController);
- 
-    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
-    function LoginController($location, AuthenticationService, FlashService) {
-        var vm = this;
- 
-        vm.login = login;
- 
-        (function initController() {
-            // reset login status
-            AuthenticationService.ClearCredentials();
-        })();
- 
-        function login() {
-            vm.dataLoading = true;
-            AuthenticationService.Login(vm.username, vm.password, function (response) {
-                if (response.success) {
-                    AuthenticationService.SetCredentials(vm.username, vm.password);
-                    $location.path('/');
-                } else {
-                    FlashService.Error(response.message);
-                    vm.dataLoading = false;
-                }
-            });
-        };
-    }
- 
-})();
+var phonecatApp = angular.module('planitFood', []);
 
+phonecatApp.controller('PhoneListCtrl', function ($scope) {
+  $scope.phones = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'DAVE XOOM™',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+});
